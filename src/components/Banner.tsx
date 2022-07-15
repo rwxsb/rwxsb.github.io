8ce4c73e5d5caa2845ee209f2db38240cc6360e4
sbecerek <suheybbecerek@gmail.com>
2022-07-15 02:12:31 +0200

    Initial design idea

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,12 +3,18 @@ import './Banner.scss'
 
 
 function Banner(){
-
-
     return (
         <div className="banner">
-            <div>
-
+            <div className="inner-banner">
+                <div className="title-wrapper">
+                    <h1>Suheyb Becerek</h1>
+                </div>
+                <div className="desription-wrapper">
+                    <p>Journey is to Journal</p>
+                </div>
+                {/* <div className="navigation-wrapper">
+                    <a href={'/about'}>About</a>
+                </div> */}
             </div>
         </div>
     );
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,7 +3,14 @@ import React from "react";
 function Content(){
     return(
         <div className="Content">
-            this will have the content
+            {/* this will have the content */}
+            {/* 
+                TODO:
+                Create a component which takes md file url,
+                parses variables from the md file 
+                and displays article card
+
+            */}
         </div>
     );
 }
